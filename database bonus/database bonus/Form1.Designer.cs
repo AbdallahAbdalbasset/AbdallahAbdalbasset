6aab38db06aed7607ebd9dfb99920953a0e7d102
AbdallahAbdalbasset <97089599+AbdallahAbdalbasset@users.noreply.github.com>
2022-01-04 11:16:02 +0200

    Add files via upload

---
+++ database bonus/database bonus/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace database_bonus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(729, 444);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(807, 34);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 295);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(878, 375);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(99, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(579, 588);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(612, 375);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(612, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(166, 292);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 612);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ database bonus/database bonus/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace database_bonus
+{
+
+    public partial class Form1 : Form
+    {
+        public int mod;
+
+        Hashing[] table = new Hashing[2000];
+
+       
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Hashing temp = new Hashing();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mod = Convert.ToInt32(textBox2.Lines[0]);
+                int check= mod/mod;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please insert the value of modulo");
+               Close();
+            }
+
+            float counter = 0;
+
+
+            for (int i = 0; i < mod; i++)
+            {
+                table[i] = new Hashing();
+                table[i].hash = i;
+
+            }
+
+            for (int i = 0; i < textBox1.Lines.Length; i++)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                string str;
+                int res;
+                
+                stopwatch.Start();
+                str = StringToBinary(textBox1.Lines[i]);
+                res = getMod(str, str.Length, mod);
+                stopwatch.Stop();
+
+                listBox1.Items.Add(stopwatch.Elapsed);
+                table[res].count++;
+                table[res].hash = res;
+                table[res].timeTake += stopwatch.Elapsed;
+                if (table[res].count > 1)
+                    counter++;
+
+
+
+            }
+            textBox3.Text = (((counter / textBox1.Lines.Length) * 100).ToString()+"%");
+            dataGridView1.DataSource = table;
+
+
+
+
+        }
+        public static string StringToBinary(string data)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in data.ToCharArray())
+            {
+                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            }
+            return sb.ToString();
+        }
+        public static int getMod(string str, int n, int k)
+        {
+            int i;
+
+            // pwrTwo[i] will store ((2^i) % k)
+            int[] pwrTwo = new int[n];
+            int res = 0;
+            try
+            {
+                pwrTwo[0] = 1 % k;
+
+
+                for (i = 1; i < n; i++)
+                {
+                    pwrTwo[i] = pwrTwo[i - 1] * (2 % k);
+                    pwrTwo[i] %= k;
+                }
+
+
+                // To store the result
+                
+                i = 0;
+                int j = n - 1;
+                while (i < n)
+                {
+
+                    // If current bit is 1
+                    if (str[j] == '1')
+                    {
+
+                        // Add the current power of 2
+                        res += (pwrTwo[i]);
+                        res %= k;
+                    }
+                    i++;
+                    j--;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Not enter null string");
+            }
+            return res;
+        }
+    }
+
+public class Hashing
+    {
+        public int hash {get;set;}
+        public int count { get; set; }
+        public TimeSpan timeTake { get; set; }
+    }
+}
